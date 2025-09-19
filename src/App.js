81d378d259eb2f3fa8518bad2af464e803cdc1c1
N.E.R.V <168000487+Hrishikesh-Gavai@users.noreply.github.com>
2025-09-19 16:10:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react'
-import Header from './components/Header'
-import ComposeEmail from './components/ComposeEmail'
-import EmailRecords from './components/EmailRecords'
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import ComposeEmail from './components/ComposeEmail';
+import EmailRecords from './components/EmailRecords';
 import StarBackground from './components/StarBackground';
-import './App.css'
+import './App.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('compose')
+  const [currentPage, setCurrentPage] = useState('compose');
+  const [theme, setTheme] = useState('dark');
+  
+  // Load theme from localStorage on component mount
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('unimail-theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.documentElement.setAttribute('data-theme', savedTheme);
+    }
+  }, []);
+  
+  // Toggle theme function
+  const toggleTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+    localStorage.setItem('unimail-theme', newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+  };
 
   return (
     <div className="App">
       <StarBackground />
-      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <Header 
+        currentPage={currentPage} 
+        setCurrentPage={setCurrentPage} 
+        toggleTheme={toggleTheme}
+        theme={theme}
+      />
       
       <div className="container">
         {currentPage === 'compose' && (
@@ -23,7 +46,7 @@ function App() {
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
