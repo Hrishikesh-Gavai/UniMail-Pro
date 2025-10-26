58f1d8af2807a006efb1a7095f44908c4e5f7b94
N.E.R.V <168000487+Hrishikesh-Gavai@users.noreply.github.com>
2025-10-27 00:05:26 +0530

    Update LoadingScreen.js

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,16 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const LoadingScreen = () => {
   return (
     <div className="loading-screen">
-      <div className="loading-logo">UniMail Pro</div>
       <div className="loading-spinner"></div>
-      <div className="loading-text">Initializing System...</div>
-      <div className="loading-progress">
-        <div className="loading-bar"></div>
-      </div>
+      <p>Loading...</p>
     </div>
-  )
-}
+  );
+};
 
-export default LoadingScreen
+export default LoadingScreen;
